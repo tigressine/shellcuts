0d0458f72916d32f007d35a3e71e162340b4caa5
Tiger Sachse <tiger@sachse.io>
2017-12-20 23:50:20 -0500

    add todo

---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+# TODO
+# Add -v version
+# Support original bashmarks commands
+# Add support for tab completion
+# Write setup script
+# Write readme
+# Get into ubuntu repos?
+# Get working with zsh
