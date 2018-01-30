d84677968448c98a9479670bc57239f4f401a8b0
Tiger Sachse <tiger@sachse.io>
2018-01-29 23:33:21 -0500

    bash support

---
+++ source/share/bash/main-function.sh
@@ -0,0 +1,13 @@
+# Part of Shellcuts by Tgsachse.
+
+# Core function of program. Sends first two arguments to sc-handler.
+# sc-handler returns a function, which is then executed.
+function sc {
+    eval "$(sc-handler $1 $2)"
+}
+
+# Full-name aliases.
+alias shellcut="sc"
+alias shellcuts="sc"
+alias shellc="sc"
+alias scut="sc"
