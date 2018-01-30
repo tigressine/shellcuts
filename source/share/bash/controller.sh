e516a8cfa092cafb0077d13360bf7d8e088a8dc9
Tiger Sachse <tgsachse@gmail.com>
2018-01-30 00:09:17 -0500

    move shellcuts.sh

--- source/share/bash/controller.sh
+++ source/share/bash/controller.sh
@@ -1,14 +1,15 @@
 # Part of Shellcuts by Tgsachse.
 
-F_FUNCT="~/.config/shellcuts/bash/main-function.sh"
-F_BASHMARKS="~/.config/shellcuts/bash/bashmarks-aliases.sh"
+# File path constants.
+F_FUNCT=~/.config/shellcuts/bash/main-function.sh
+F_BASHMARKS=~/.config/shellcuts/bash/bashmarks-aliases.sh
 
 # If main function file exists, load it.
-if [ -f $F_FUNCT ]; then
+if [ -f "$F_FUNCT" ]; then
     . $F_FUNCT
 fi
 
 # If Bashmarks syntax is enabled (e.g. the file exists), load it.
-if [ -f $F_BASHMARKS ]; then
+if [ -f "$F_BASHMARKS" ]; then
     . $F_BASHMARKS
 fi
