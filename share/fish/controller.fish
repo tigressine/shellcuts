5bcb0d4633bbf7a378cb23715041c89a97d530c7
Tiger Sachse <tiger@sachse.io>
2018-02-10 20:10:09 -0500

    consolidating

--- share/fish/controller.fish
+++ share/fish/controller.fish
@@ -1,13 +1,19 @@
 # Part of Shellcuts by Tgsachse.
 
 # File path constants.
-set F_FUNCT ~/.config/shellcuts/fish/main-function.fish
 set F_BASHMARKS ~/.config/shellcuts/fish/bashmarks-aliases.fish
 
-# If main function file exists, load it.
-if test -e $F_FUNCT
-    . $F_FUNCT
+# Core function of program. Sends first two arguments to sc-handler.
+# sc-handler returns a function, which is then executed.
+function sc
+    eval (python3 sc-handler $argv)
 end
+
+# Full-name aliases.
+alias shellcut="sc"
+alias shellcuts="sc"
+alias shellc="sc"
+alias scut="sc"
 
 # If Bashmarks syntax is enabled (e.g. the file exists), load it.
 if test -e $F_BASHMARKS
