16734eff1b5873ad42a07dbd218ebd01d568e05f
Tiger Sachse <tiger@sachse.io>
2018-02-25 22:44:25 -0500

    workaround for fish removing newlines

--- share/fish/controller.fish
+++ share/fish/controller.fish
@@ -6,7 +6,9 @@ set F_BASHMARKS ~/.config/shellcuts/fish/bashmarks-aliases.fish
 # Core function of program. Sends first two arguments to sc-handler.
 # sc-handler returns a function, which is then executed.
 function sc
+    set -l IFS
     eval (python3 /usr/bin/sc-handler $argv)
+    set -e IFS
 end
 
 # Full-name aliases.
