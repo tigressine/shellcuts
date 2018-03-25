fba7436bb35cfee396d20c72353129cd8b414f5d
Tiger Sachse <tgsachse@gmail.com>
2018-03-25 14:33:49 -0400

    changes to share structure to support more plugins

--- share/bash/controller.sh
+++ share/bash/controller.sh
@@ -1,7 +1,8 @@
 # Part of Shellcuts by Tgsachse.
 
-# File path constants.
-F_BASHMARKS=~/.config/shellcuts/bash/bashmarks-aliases.sh
+# Absolute location of the plugins folder.
+#PLUGINS="$HOME/.config/shellcuts/bash/plugins"
+PLUGINS="$HOME/Dropbox/Code/Shellcuts/share/bash/plugins"
 
 # Core function of program. Sends first two arguments to sc-handler.
 # sc-handler returns a function, which is then executed.
@@ -9,13 +10,7 @@ function sc {
     eval "$(python3 /usr/bin/sc-handler $1 $2)"
 }
 
-# Full-name aliases.
-alias shellcut="sc"
-alias shellcuts="sc"
-alias shellc="sc"
-alias scut="sc"
-
-# If Bashmarks syntax is enabled (e.g. the file exists), load it.
-if [ -f "$F_BASHMARKS" ]; then
-    . $F_BASHMARKS
-fi
+# Sources all files located in the plugins folder.
+for FILE in $PLUGINS/* ; do
+    . $FILE
+done
