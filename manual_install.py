06978ee27d420711538a9e911141bc9d3b48b4ae
Tiger Sachse <tiger@sachse.io>
2018-03-28 17:22:41 -0400

    check filesystem

--- manual_install.py
+++ manual_install.py
@@ -1,8 +1,18 @@
 """
 """
+import compileall
 from sys import argv as args
+from pathlib import Path
 
-def check_if_local_install():
+paths = {
+    'bin' : Path('/usr/bin'),
+    'share' : Path('/usr/share'),
+    'trigger' : Path('/home/tgsachse/Dropbox/Code/Shellcuts/junk')
+}
+
+def check_local_install():
+    """
+    """
     if len(args) > 1:
         if args[1] == '--local':
             return True
@@ -14,11 +24,33 @@ def check_if_local_install():
     else:
         return False
 
+def check_paths():
+    """
+    """
+    for key, path in paths.items():
+        if not path.exists():
+            return False
+    else:
+        return True
+
 def install_global():
+    """
+    """
     pass
 
 def install_local():
-    pass
+    """
+    """
+    
 
 def main():
-    local = check_if_local_install()
+    """
+    """
+    if check_local_install():
+        install_local()
+    elif check_paths():
+        install_global()
+    else:
+        print("Filesystem unsupported. Try installing locally.")
+
+main()
