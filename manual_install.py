7e997a0c06b955fef1f01cd5db825b25a9bb2c3d
Tiger Sachse <tgsachse@gmail.com>
2018-03-28 17:52:42 -0400

    working on pythonically downloading and unzipping

--- manual_install.py
+++ manual_install.py
@@ -3,11 +3,14 @@
 import compileall
 from sys import argv as args
 from pathlib import Path
+import subprocess
+from tarfile import TarFile
 
+VERSION = 'v1.2.1'
 paths = {
     'bin' : Path('/usr/bin'),
     'share' : Path('/usr/share'),
-    'trigger' : Path('/home/tgsachse/Dropbox/Code/Shellcuts/junk')
+    #'trigger' : Path('/home/tgsachse/Dropbox/Code/Shellcuts/junk')
 }
 
 def check_local_install():
@@ -33,15 +36,49 @@ def check_paths():
     else:
         return True
 
-def install_global():
+def check_package_managers():
     """
     """
     pass
 
+def install_global():
+    """
+    """
+    check_package_managers()
+
 def install_local():
     """
     """
+    D_SHELLCUTS = Path('~/.shellcuts').expanduser()
+    D_SHELLCUTS.mkdir(parents=True, exist_ok=True)
+    DEST = D_SHELLCUTS.joinpath('git_temp')
+    command = ('wget', 
+               'https://github.com/tgsachse/shellcuts/archive/{}.tar.gz'.format(VERSION),
+               '-P',
+               str(DEST))#check this isnt possible with a lib
+
+    subprocess.run(command)
+    DEST = DEST.joinpath('{}.tar.gz'.format(VERSION))
+    TarFile(str(DEST)).extractall()
+
+    LOCAL_TREE = (
+        ('bin', 'bin/'),
+        ('share', 'share/'),
+        ('docs', 'docs/'),
+        ('man', 'man/man1')
+    )
+
     
+
+    # make sure weve got this bitch downloaded
+    # compile the binaries
+    # move bytecode to bin
+    # move share tree to share
+    # move docs to docs
+    # mkdir for man and move man to man
+    ####### add to bash plugin and rest to update bin path
+    # destroy git
+
 
 def main():
     """
