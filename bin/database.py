f7522e9b7a8c584cf21f6f892a03583d455b5ea4
Tiger Sachse <tiger@sachse.io>
2018-05-24 14:58:13 -0400

    added docstrings and refactored

--- bin/database.py
+++ bin/database.py
@@ -1,83 +1,98 @@
 import sqlite3
 from pathlib import Path
 
-class Database:
+class DatabaseConnection:
+    """An SQLite database connection containing shellcuts."""
     def __init__(self, path):
+        """Save path of database as self.path."""
         self.path = path
 
     def __enter__(self):
+        """Initialize connection and cursor."""
         if Path(self.path).is_file():
             self.connection = sqlite3.connect(self.path)
             self.cursor = self.connection.cursor()
         else:
             self.connection = sqlite3.connect(self.path)
             self.cursor = self.connection.cursor()
-            self.create() 
+            self.create()
 
         return self
 
     def __exit__(self, type, value, traceback):
+        """Save changes to database and close connection."""
         self.connection.commit()
         self.connection.close()
 
     def flush(self):
-        self.cursor.execute("DELETE FROM shellcuts")
-        self.cursor.execute("DELETE FROM default_command")
+        """Delete all data from database."""
+        self.cursor.execute("DELETE FROM table_shellcuts")
+        self.cursor.execute("DELETE FROM table_defaults")
         
     def create(self):
-        self.cursor.execute("""CREATE TABLE shellcuts (name TEXT,
-                                                  path TEXT,
-                                                  command TEXT)""")
-        self.cursor.execute("""CREATE TABLE default_command (enabled BOOLEAN,
-                                                        command TEXT)""")
-        self.toggle_default(False)
+        """Create neccessary tables in database."""
+        self.cursor.execute("""CREATE TABLE table_shellcuts (name TEXT,
+                                                             path TEXT,
+                                                             command TEXT)""")
+        self.cursor.execute("""CREATE TABLE table_defaults (enabled BOOLEAN,
+                                                            command TEXT)""")
+        self.toggle_default_commands(False)
 
-    def toggle_default(self, boolean, command=""):
-        self.cursor.execute("SELECT * FROM default_command")
+    def toggle_default_commands(self, enabled, command=""):
+        """Toggle default flag in table_defaults and set command if true."""
+        self.cursor.execute("SELECT * FROM table_defaults")
+        
         if self.cursor.fetchone() is None:
-            self.cursor.execute("INSERT INTO default_command VALUES (?,?)", (boolean, command))
+            self.cursor.execute("INSERT INTO table_defaults VALUES (?,?)",
+                                (enabled, command))
+        elif enabled is True:
+            self.cursor.execute("UPDATE table_defaults SET enabled=?, command=?",
+                                (enabled, command))
         else:
-            self.cursor.execute("UPDATE default_command SET enabled=?", (boolean,))
-            if command != "":
-                self.cursor.execute("UPDATE default_command SET command=?", (command,))
+            self.cursor.execute("UPDATE table_defaults SET enabled=?",
+                                (enabled,))
 
-    def get_default(self):
-        self.cursor.execute("SELECT * FROM default_command")
+    def get_default_command(self):
+        """Get the default command from table_defaults."""
+        self.cursor.execute("SELECT command FROM table_defaults")
 
         return self.cursor.fetchone()
 
     def insert_shellcut(self, name, path):
+        """Insert shellcut into database."""
+
+        # If the shellcut already exists in the table, delete it first.
         if self.get_shellcut_path(name) is not None:
             self.delete_shellcut(name)
 
-        self.cursor.execute("INSERT INTO shellcuts VALUES (?,?,?)", (name, path, ""))
+        self.cursor.execute("INSERT INTO table_shellcuts VALUES (?,?,?)",
+                            (name, path, ""))
 
     def get_shellcut_path(self, name):
-        self.cursor.execute("SELECT path FROM shellcuts WHERE name=?", (name,))
+        """Get path of named shellcut."""
+        self.cursor.execute("SELECT path FROM table_shellcuts WHERE name=?",
+                            (name,))
 
         return self.cursor.fetchone()
 
     def get_shellcut(self, name):
-        self.cursor.execute("SELECT * FROM shellcuts WHERE name=?", (name,))
+        """Get path, name, and command of named shellcut."""
+        self.cursor.execute("SELECT * FROM table_shellcuts WHERE name=?",
+                            (name,))
 
         return self.cursor.fetchone()
 
     def get_all_shellcuts(self):
-        self.cursor.execute("SELECT * FROM shellcuts")
+        """Get all shellcuts in database."""
+        self.cursor.execute("SELECT * FROM table_shellcuts")
 
         return self.cursor.fetchall()
 
     def delete_shellcut(self, name):
-        self.cursor.execute("DELETE FROM shellcuts WHERE name=?", (name,))
+        """Delete a shellcut from the database."""
+        self.cursor.execute("DELETE FROM table_shellcuts WHERE name=?", (name,))
 
-with Database("hello.db") as database:
-    '''database.insert_shellcut("hello", "~/hello")
-    database.insert_shellcut("hjello", "~/jaylo")
-    database.insert_shellcut("goodbye", "~/goodbye")
-    database.insert_shellcut("destroyme", "~/destroyme")
-    database.insert_shellcut("restinpeace", "/home/tgsachse/Dropbox/Code/Shellcuts/")
-    '''
-    
+with DatabaseConnection("hello.db") as database:
     database.insert_shellcut("f", "/f")
     print(database.get_shellcut_path("restinpeace"))
     print(database.get_shellcut("destroyme"))
@@ -86,8 +101,8 @@ with Database("hello.db") as database:
     
     print(database.get_all_shellcuts())
     
-    print(database.get_default())
-    database.toggle_default(True, command="clsa")
-    print(database.get_default())
-    database.toggle_default(False)
-    print(database.get_default())
+    print(database.get_default_command())
+    database.toggle_default_commands(True, command="clsa")
+    print(database.get_default_command())
+    database.toggle_default_commands(False)
+    print(database.get_default_command())
