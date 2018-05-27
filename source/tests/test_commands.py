7ed672342c7eb0781931ffc6254dd2e0b2b1d54e
Tiger Sachse <tiger@sachse.io>
2018-05-27 12:24:22 -0400

    move and delete checks

--- source/tests/test_commands.py
+++ source/tests/test_commands.py
@@ -10,6 +10,11 @@ SHELLCUTS_FILE = Path('/tmp/shellcuts_database.db')
 
 class CommandTester(unittest.TestCase):
     """"""
+    def setUp(self):
+        """"""
+        if SHELLCUTS_FILE.is_file():
+            SHELLCUTS_FILE.unlink()
+
     @patch('core.utils.VERSION_FILE', VERSION_FILE)
     @patch('sys.stdout', new_callable=StringIO)
     def test_000_check_version_command(self, stream):
@@ -51,9 +56,6 @@ class CommandTester(unittest.TestCase):
     @patch('sys.stdout', new_callable=StringIO)
     def test_003_check_new_command(self, stream, mock_getcwd):
         """Confirm that command_new adds a shellcut to a database."""
-        if SHELLCUTS_FILE.is_file():
-            SHELLCUTS_FILE.unlink()
-        
         command_new('test1')
 
         # Check that a courtesy message is printed to the screen.
@@ -76,3 +78,65 @@ class CommandTester(unittest.TestCase):
             self.assertEqual(db.get_shellcut('test1')[0], 'test1')
             self.assertEqual(db.get_shellcut_path('test1'), '/tmp/test2/')
             self.assertEqual(len(db.get_all_shellcuts()), 1)
+
+    @patch('core.commands.SHELLCUTS_FILE', SHELLCUTS_FILE)
+    @patch('os.getcwd', return_value='/tmp/test1/')
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_004_check_move_command(self, stream, mock_getcwd):
+        command_move('test1')
+
+        # Move will function as the new command if the shellcut doesn't exist.
+        self.assertTrue(re.search('Moved shellcut \'test1\'', stream.getvalue()))
+
+        # Check that the new shellcut is there.
+        with DatabaseConnection(SHELLCUTS_FILE) as db:
+            self.assertEqual(db.get_shellcut('test1')[0], 'test1')
+            self.assertEqual(db.get_shellcut_path('test1'), '/tmp/test1/')
+
+        # Change the working directory.
+        mock_getcwd.return_value = '/tmp/test2/'
+
+        command_move('test1')
+
+        # Now there should only be one shellcut pointing to the latest directory.
+        with DatabaseConnection(SHELLCUTS_FILE) as db:
+            self.assertEqual(db.get_shellcut('test1')[0], 'test1')
+            self.assertEqual(db.get_shellcut_path('test1'), '/tmp/test2/')
+            self.assertEqual(len(db.get_all_shellcuts()), 1)
+
+    
+    @patch('core.commands.SHELLCUTS_FILE', SHELLCUTS_FILE)
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_005_check_delete_command(self, stream):
+        """Confirm that the delete command works properly."""
+        # Create a database with 3 entries.
+        with DatabaseConnection(SHELLCUTS_FILE) as db:
+            db.insert_shellcut('test1', '/tmp/test1')
+            db.insert_shellcut('test2', '/tmp/test2')
+            db.insert_shellcut('test3', '/tmp/test3')
+            self.assertEqual(len(db.get_all_shellcuts()), 3)
+
+        command_delete('test2')
+
+        # Confirm that the deletion message is printed.
+        self.assertTrue(re.search('Deleted shellcut \'test2\'', stream.getvalue()))
+
+        # Confirm that only test2 was removed from the database.
+        with DatabaseConnection(SHELLCUTS_FILE) as db:
+            self.assertEqual(db.get_shellcut('test1')[0], 'test1') 
+            self.assertIsNone(db.get_shellcut('test2'))
+            self.assertEqual(db.get_shellcut('test3')[0], 'test3')
+
+        # Attempt to remove something from the database that isn't there.
+        command_delete('test4')
+
+        # Confirm that the deletion message shows anyway.
+        self.assertTrue(re.search('Deleted shellcut \'test4\'', stream.getvalue()))
+
+        # Check that nothing was deleted.
+        with DatabaseConnection(SHELLCUTS_FILE) as db:
+            self.assertEqual(len(db.get_all_shellcuts()), 2)
+
+
+
+
