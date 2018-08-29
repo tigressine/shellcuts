5b94880313a8118d235cfc6057de6fcca6065202
Tiger Sachse <tiger@sachse.io>
2018-08-29 19:13:40 -0400

    shells reworked

--- shells/fish/controller.fish
+++ shells/fish/controller.fish
@@ -1,23 +1,17 @@
 # Part of Shellcuts by Tgsachse.
 
-# File path constants.
-set F_BASHMARKS ~/.config/shellcuts/fish/bashmarks-aliases.fish
+set HANDLER "~/.shellcuts/binaries/shellcuts.pyc"
+set PLUGINS "~/.shellcuts/shells/fish/plugins/*"
 
-# Core function of program. Sends first two arguments to sc-handler.
-# sc-handler returns a function, which is then executed.
+# Get a command from the Python handler, based on
+# the given inputs, then execute that command.
 function sc
     set -l IFS
-    eval (python3 /usr/bin/sc-handler $argv)
+    eval (python3 $HANDLER $argv)
     set -e IFS
 end
 
-# Full-name aliases.
-alias shellcut="sc"
-alias shellcuts="sc"
-alias shellc="sc"
-alias scut="sc"
-
-# If Bashmarks syntax is enabled (e.g. the file exists), load it.
-if test -e $F_BASHMARKS
-    . $F_BASHMARKS
+# Load all shell plugins.
+for FILE in $PLUGINS
+    . $FILE
 end
--- shells/zsh/plugins/full_name_aliases.sh
+++
@@ -1,7 +0,0 @@
-# Part of Shellcuts by Tiger Sachse.
-
-# Full-name aliases.
-alias shellcut="sc"
-alias shellcuts="sc"
-alias shellc="sc"
-alias scut="sc"
