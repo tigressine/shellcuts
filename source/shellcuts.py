4ec3d521a614286f637b5aea8627ab654be74460
Tiger Sachse <tiger@sachse.io>
2018-08-30 22:46:04 -0400

    bug showing list

--- source/shellcuts.py
+++ source/shellcuts.py
@@ -11,9 +11,7 @@ commander = Commander(utilities.VERSION_FILE,
 parser = Parser(commander)
 parser.parse_arguments()
 
-if parser.arguments.name is None:
-    utilities.throw_help()
-elif len(parser.unknown) <= 0:
+if len(parser.unknown) <= 0:
     commander.go(parser.arguments.name)
     exit(0)
 
