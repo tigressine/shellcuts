2728f2c1e8edfc6bf364534f5defe7fad9f0ee3f
Tiger <tgsachse@gmail.com>
2018-08-31 12:04:42 -0400

    prevent naming conflicts

--- shells/bash/controller.sh
+++ shells/bash/controller.sh
@@ -1,15 +1,15 @@
 # Part of Shellcuts by Tgsachse.
 
-HANDLER="~/.shellcuts/binaries/shellcuts.pyc"
-PLUGINS="~/.shellcuts/shells/bash/plugins/*"
+SHELLCUTS_HANDLER="~/.shellcuts/binaries/shellcuts.pyc"
+SHELLCUTS_PLUGINS="~/.shellcuts/shells/bash/plugins/*"
 
 # Get a command from the Python handler, based on
 # the given inputs, then execute that command.
 function sc {
-    eval "$(python3 $HANDLER $1 $2)"
+    eval "$(python3 $SHELLCUTS_HANDLER $1 $2)"
 }
 
 # Load all shell plugins.
-for FILE in $PLUGINS; do
+for FILE in $SHELLCUTS_PLUGINS; do
     . $FILE
 done
--- shells/zsh/controller.sh
+++ shells/zsh/controller.sh
@@ -1,15 +1,15 @@
 # Part of Shellcuts by Tgsachse.
 
-HANDLER="~/.shellcuts/binary/shellcuts.pyc"
-PLUGINS="~/.shellcuts/config/zsh/plugins/*"
+SHELLCUTS_HANDLER="~/.shellcuts/binary/shellcuts.pyc"
+SHELLCUTS_PLUGINS="~/.shellcuts/config/zsh/plugins/*"
 
 # Get a command from the Python handler, based on
 # the given inputs, then execute that command.
 function sc {
-    eval "$(python3 $HANDLER $1 $2)"
+    eval "$(python3 $SHELLCUTS_HANDLER $1 $2)"
 }
 
 # Load all shell plugins.
-for FILE in $PLUGINS; do
+for FILE in $SHELLCUTS_PLUGINS; do
     . $FILE
 done
