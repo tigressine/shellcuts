20c18c333c96e104f30177e2720372d318275850
Tiger Sachse <tiger@sachse.io>
2018-08-31 12:04:42 -0400

    prevent naming conflicts

--- shells/fish/controller.fish
+++ shells/fish/controller.fish
@@ -1,17 +1,17 @@
 # Part of Shellcuts by Tgsachse.
 
-set HANDLER "~/.shellcuts/binaries/shellcuts.pyc"
-set PLUGINS "~/.shellcuts/shells/fish/plugins/*"
+set SHELLCUTS_HANDLER "~/.shellcuts/binaries/shellcuts.pyc"
+set SHELLCUTS_PLUGINS "~/.shellcuts/shells/fish/plugins/*"
 
 # Get a command from the Python handler, based on
 # the given inputs, then execute that command.
 function sc
     set -l IFS
-    eval (python3 $HANDLER $argv)
+    eval (python3 $SHELLCUTS_HANDLER $argv)
     set -e IFS
 end
 
 # Load all shell plugins.
-for FILE in $PLUGINS
+for FILE in $SHELLCUTS_PLUGINS
     . $FILE
 end
