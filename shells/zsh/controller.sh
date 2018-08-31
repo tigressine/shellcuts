e9f9349d684b03de37f082a4c0f68f7f3bfbe952
Tiger <tgsachse@gmail.com>
2018-08-31 16:57:10 -0400

    shell variables are weird

--- shells/zsh/controller.sh
+++ shells/zsh/controller.sh
@@ -1,15 +1,12 @@
 # Part of Shellcuts by Tgsachse.
 
-SHELLCUTS_HANDLER="~/.shellcuts/binary/shellcuts.pyc"
-SHELLCUTS_PLUGINS="~/.shellcuts/config/zsh/plugins/*"
-
 # Get a command from the Python handler, based on
 # the given inputs, then execute that command.
 function sc {
-    eval "$(python3 $SHELLCUTS_HANDLER $1 $2)"
+    eval "$(python3 $HOME/.shellcuts/binaries/shellcuts.pyc $1 $2)"
 }
 
 # Load all shell plugins.
-for FILE in $SHELLCUTS_PLUGINS; do
+for FILE in $HOME/.shellcuts/shells/zsh/plugins/*; do
     . $FILE
 done
