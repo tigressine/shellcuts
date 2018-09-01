43ca6278819d6ec513acbca293377064bdae9a14
Tiger <tgsachse@gmail.com>
2018-09-01 14:15:07 -0400

    final comments on existing code

--- install.py
+++ install.py
@@ -1,4 +1,9 @@
-"""
+"""Local installation script for Shellcuts.
+
+This script should work to install Shellcuts on any system with a Unix-like
+file organization scheme (e.g. Linux and macOS).
+
+Part of Shellcuts by Tiger Sachse.
 """
 import re
 import shutil
@@ -33,26 +38,29 @@ DESTINATION_DIRS = {
     'core' : Path('~/.shellcuts/binaries/core').expanduser(),
 }
 
-# Save any existing json file
+# add a thing to confirm that all the files exist
+
+# If a shellcuts JSON file already exists, temporarily move it to save it.
 if SHELLCUTS_JSON.exists():
     SHELLCUTS_JSON.replace(TEMPORARY_JSON)
 
-# remove old installations
+# Remove any old installation files.
 shutil.rmtree(str(DESTINATION_DIRS['shellcuts']))
 
-# copy directories
+# Copy the source directories to the installation location.
 DESTINATION_DIRS['shellcuts'].mkdir(parents=True)
 for directory in SOURCE_DIRS:
     shutil.copytree(Path(directory), DESTINATION_DIRS[directory])
 
-# make any directories we missed in copying
+# Create other destination-only directories that will be used later.
 for directory in DESTINATION_DIRS.keys():
     DESTINATION_DIRS[directory].mkdir(exist_ok=True)
 
-# compile everything
+# Compile all of the installed source code.
 compileall.compile_dir(str(DESTINATION_DIRS['source']))
 
-# move bytecode to binaries
+# Move all compiled top-level files into the binaries folder, then delete the
+# compilation cache.
 top_cache = DESTINATION_DIRS['source'] / CACHE
 for binary in top_cache.iterdir():
     binary_match = re.match(COMPILE_PATTERN, binary.name)
@@ -60,6 +68,8 @@ for binary in top_cache.iterdir():
     binary.replace(DESTINATION_DIRS['binaries'] / target)
 top_cache.rmdir()
 
+# Move all compiled core package files into the binaries/core folder, then
+# delete the compilation cache.
 core_cache = DESTINATION_DIRS['source'] / CORE / CACHE
 for binary in core_cache.iterdir():
     binary_match = re.match(COMPILE_PATTERN, binary.name)
@@ -67,8 +77,12 @@ for binary in core_cache.iterdir():
     binary.replace(DESTINATION_DIRS['binaries'] / CORE / target)
 core_cache.rmdir()
 
-# add configs as needed.
+# Append example shell configuration instructions to existing shell
+# configuration files. These configuration instructions allow Shellcuts to
+# hook into the shell correctly.
 for shell in SHELLS:
+
+    # Only append to configuration files that exist.
     if SHELL_CONFIGS[shell].exists():
         with open(SHELL_EXAMPLES[shell], 'r') as f:
             new_lines = f.readlines()
@@ -76,6 +90,8 @@ for shell in SHELLS:
         with open(SHELL_CONFIGS[shell], 'r') as f:
             shell_lines = f.readlines()
 
+            # If the current configuration file already has the necessary
+            # instructions (from a previous install) then don't add them again.
             for new_line in new_lines:
                 if new_line not in shell_lines:
                     missing_new_lines = True
@@ -83,12 +99,12 @@ for shell in SHELLS:
             else:
                 missing_new_lines = False
 
-        print(shell, missing_new_lines)
+        # if the instructions are not in the configuration file, add them.
         if missing_new_lines:
             with open(SHELL_CONFIGS[shell], 'a') as f:
                 for new_line in new_lines:
                     f.write(new_line)
 
-# if a temporary file was saved, move
+# Move the old shellcuts JSON back into place.
 if TEMPORARY_JSON.exists():
     TEMPORARY_JSON.replace(SHELLCUTS_JSON)
--- source/core/commander.py
+++ source/core/commander.py
@@ -1,11 +1,10 @@
-"""Print commands associated with Shellcuts' options.
+"""Print shell commands based on given arguments.
 
-All printed shell commands are captured by the shell script that calls
-shellcuts.py, where they are then executed by the shell script. This is
-necessary because the 'cd' command moves the user only if it is called by the
-user (or a shell script that the user has directly invoked). Any calls to 'cd',
-or even system calls to 'chdir', will change the working directory of the
-script but not the user.
+All printed commands are captured and evaluated by the shell script that calls
+this file. This method is necessary because the 'cd' command moves the user
+only if it is called by the user (or a shell script that the user has directly
+invoked). Any calls in Python programs to 'cd', or even system calls to 'chdir',
+will change the working directory of the program but not the user.
 
 Part of Shellcuts by Tiger Sachse.
 """
@@ -17,14 +16,18 @@ from core.jsonary import Jsonary
 class Commander:
     """A class that holds all possible commands for Shellcuts."""
     def __init__(self, version_file, shellcuts_file, manual_file):
-        """Initialize with external, saved information."""
+        """Initialize with external, saved information.
+        
+        The jsonary loads shellcuts from an external JSON file into a
+        dictionary structure.
+        """
         self.manual_file = manual_file
         self.version_file = version_file
         self.shellcuts = Jsonary(shellcuts_file)
 
 
     def execute(self, arguments):
-        """Call the appropriate function based on the given arguments."""
+        """Call the appropriate function based on given arguments."""
         if arguments.new:
             self.new(arguments.new)
         elif arguments.delete:
