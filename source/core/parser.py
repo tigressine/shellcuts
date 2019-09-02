0f71db1522761ed465f3c20470b19f34022be724
Tiger Sachse <tiger@sachse.io>
2019-09-02 16:38:37 -0700

    Print out follow commands with list.

--- source/core/parser.py
+++ source/core/parser.py
@@ -7,7 +7,7 @@ from core import utilities
 
 class Parser(argparse.ArgumentParser):
     """Class to handle command line arguments."""
-    
+
     def __init__(self, *args, **kwargs):
         """Initialize super and add a base argument for short-circuiting."""
         super().__init__(*args, add_help=False, **kwargs)
@@ -26,14 +26,14 @@ class Parser(argparse.ArgumentParser):
 
     def add_arguments(self):
         """Add flags to the parser."""
-        self.add_argument('--unfollow')
+        self.add_argument('-u', '--unfollow')
         self.add_argument('-m', '--move')
         self.add_argument('-p', '--print')
         self.add_argument('-d', '--delete')
         self.add_argument('-n', '--new', nargs='+')
         self.add_argument('-f', '--follow', nargs='+')
         self.add_argument('--man', action='store_true', default=None)
-        self.add_argument('--version', action='store_true', default=None)
+        self.add_argument('-v', '--version', action='store_true', default=None)
         self.add_argument('-h', '--help', action='store_true', default=None)
         self.add_argument('-l', '--list', action='store_true', default=None)
         self.add_argument('-c', '--crumb', action='store_true', default=None)
