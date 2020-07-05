c01da732ccc0f145e5b8b5a01db6fa52f3e0a519
Tiger Sachse <tiger@sachse.io>
2020-07-05 12:18:46 -0700

    Simplify shell configuration.

--- shells/bash/plugins/bashmarks_aliases.bash
+++
@@ -1,8 +0,0 @@
-# Part of Shellcuts by Tgsachse.
-
-# Aliases that emulate the original Bashmarks syntax.
-alias g="sc"
-alias s="sc -n"
-alias p="sc -p"
-alias d="sc -d"
-alias l="sc -l"
--- shells/fish/plugins/descriptive_aliases.fish
+++
@@ -1,8 +0,0 @@
-# Part of Shellcuts by Tgsachse.
-
-# Provide longer, more descriptive versions
-# of the sc command.
-alias scut="sc"
-alias shellc="sc"
-alias shellcut="sc"
-alias shellcuts="sc"
