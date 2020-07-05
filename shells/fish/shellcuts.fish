eb82cc36125adfca9708177cad21f30e1bcec1f4
Tiger Sachse <tgsachse@gmail.com>
2020-07-05 12:18:46 -0700

    Simplify shell configuration.

---
+++ shells/fish/shellcuts.fish
@@ -0,0 +1,6 @@
+# Process arguments with sc-core, then evaluate the result as a shell command.
+function sc
+    set -l IFS
+    eval (sc-core $argv)
+    set -e IFS
+end
---
+++ shells/posix/shellcuts.sh
@@ -0,0 +1,4 @@
+# Process arguments with sc-core, then evaluate the result as a shell command.
+function sc {
+    eval "$(sc-core "$@")"
+}
